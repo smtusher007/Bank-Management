48c6d93fbcd19e682b766637a89bfc1081f0ce9d
S M Tusher <85401497+smtusher007@users.noreply.github.com>
2022-10-23 15:51:31 +0600

    Updated

--- 3. InheritanceDemo/Circle.java
+++
@@ -1,9 +0,0 @@
-import java.lang.*;
-
-public class Circle extends Shape
-{
-		
-	public Circle(){System.out.println("Empty-Circle");}
-	
-	
-}
--- 3. InheritanceDemo/Square.java
+++
@@ -1,8 +0,0 @@
-import java.lang.*;
-
-public class Square extends Shape
-{
-	public Square(){System.out.println("Empty-Square");}
-
-	
-}
