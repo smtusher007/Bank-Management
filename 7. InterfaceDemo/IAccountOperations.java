c3780a51129e701f6fb9039505d5a7a2fab7f1b4
S M Tusher <85401497+smtusher007@users.noreply.github.com>
2022-10-23 15:51:49 +0600

    Updated

--- 7. InterfaceDemo/IAccountOperations.java
+++
@@ -1,9 +0,0 @@
-import java.lang.*;
-
-public interface IAccountOperations
-{
-	void addAccount(Account a);
-	void removeAccount(Account a);
-	Account searchAccount(int accountNumber);
-	void showAllAccounts();
-}
