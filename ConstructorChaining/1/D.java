afc9ff7b58033512aecc95945799efc27b7e9c61
S M Tusher <85401497+smtusher007@users.noreply.github.com>
2022-10-23 15:52:35 +0600

    Updated

--- ConstructorChaining/1/D.java
+++
@@ -1,8 +0,0 @@
-import java.lang.*;
-
-public class D extends B
-{
-	public D(){System.out.println("E-D");}
-	public D(int d1){System.out.println("P-D1");}
-	public D(int d1, int d2){System.out.println("P-D2");}
-}
--- ConstructorChaining/2/A.java
+++
@@ -1,8 +0,0 @@
-import java.lang.*;
-
-public class A
-{
-	public A(){System.out.println("E-A");}
-	public A(int a1){System.out.println("P-A1");}
-	public A(int a1, int a2){System.out.println("P-A2");}
-}
